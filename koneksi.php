103b58916f4d236eb75d4752c5360029c1b6d19a
Satrio-Bagus <satrio.baguswbw@gmail.com>
2022-01-26 17:06:51 +0700

    change mysql user and pass

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
     // nama host, Username, password dan nama database
-    $koneksi = mysqli_connect("localhost","root","","pwpb_laundry");
+    $koneksi = mysqli_connect("localhost","bagus","1sampai8","pwpb_laundry");
     // Periksa Koneksi
     if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
